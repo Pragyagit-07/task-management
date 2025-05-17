acea3e8003139b7653430952350678c72e21d5b0
{Pragyagit-07} <{pragyamakadariya7@gmail.com}>
2025-05-18 00:42:28 +0530

    commit

--- Task-manager-backend/index.js
+++ Task-manager-backend/index.js
@@ -18,6 +18,10 @@ const allowedOrigins = [
 const corsOptions = {
   origin: allowedOrigins,
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 };
 // // CORS options
 // const corsOptions = {
