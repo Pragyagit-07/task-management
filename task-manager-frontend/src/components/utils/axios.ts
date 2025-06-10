734315a2d44d4e010053cf85f20356b3b6ead269
{Pragyagit-07} <{pragyamakadariya7@gmail.com}>
2025-06-10 23:01:30 +0530

    remove

--- task-manager-frontend/src/components/utils/axios.ts
+++ task-manager-frontend/src/components/utils/axios.ts
@@ -1,7 +1,7 @@
 // src/components/utils/axios.ts
 
  import axios from 'axios';
- const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
 const instance = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
