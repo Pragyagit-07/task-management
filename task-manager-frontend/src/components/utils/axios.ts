ca2a56d18a2773ca20c0688e7b1475f338da7ef0
{Pragyagit-07} <{pragyamakadariya7@gmail.com}>
2025-06-10 23:37:35 +0530

    new change

--- task-manager-frontend/src/components/utils/axios.ts
+++ task-manager-frontend/src/components/utils/axios.ts
@@ -1,7 +1,9 @@
 // src/components/utils/axios.ts
 
  import axios from 'axios';
- const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+ const API_BASE_URL = 'https://task-management-sq7a.onrender.com';
+
+//  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
 const instance = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
