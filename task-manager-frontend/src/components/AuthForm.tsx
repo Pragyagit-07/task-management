5dee16974be7ea45bc39aac3d90e9ee524febe9d
{Pragyagit-07} <{pragyamakadariya7@gmail.com}>
2025-06-11 21:48:43 +0530

    remove the forget password

--- task-manager-frontend/src/components/AuthForm.tsx
+++ task-manager-frontend/src/components/AuthForm.tsx
@@ -84,15 +84,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
         >
           {login ? 'Login' : 'Register'}
         </button>
-        <input
-  type="password"
-  placeholder="Password"
-  className="w-full border px-4 py-2 rounded"
-  value={password}
-  onChange={(e) => setPassword(e.target.value)}
-  required
-/>
-
+        
 {login && (
   <p className="text-sm mt-2 text-right">
     <a href="/forgot-password" className="text-blue-600 underline">
