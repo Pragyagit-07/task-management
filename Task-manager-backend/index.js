597888756ce2b2a07f468c725c48ec40fc06ab3b
{Pragyagit-07} <{pragyamakadariya7@gmail.com}>
2025-06-11 09:43:26 +0530

    emove regex path

--- Task-manager-backend/index.js
+++ Task-manager-backend/index.js
@@ -34,7 +34,7 @@ const corsOptions = {
    optionsSuccessStatus: 200 
 };
 app.use(cors(corsOptions)); 
-app.options('*', cors(corsOptions));
+
 
 app.use(express.json());
 // Log origin for debugging
